60546923e8cec038c7641a3794cb1d3d8f1b03c0
Tomasz22 <tomas.kawecki27@outlook.com>
2021-02-22 11:47:14 +0100

    I have changed Learn React to Welcome to Codaisseur

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Some changes !!!
-        
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Codaisseur
+        
         </a>
       </header>
     </div>
